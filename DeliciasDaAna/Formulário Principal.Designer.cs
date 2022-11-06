d15aae9f2611f2161360de4af6259ff65818b673
Miguel <Miguel@DESKTOP-7MBEGR9>
2022-11-06 19:08:34 -0300

    Adicionar arquivos de projeto.

---
+++ DeliciasDaAna/Formulário Principal.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace DeliciasDaAna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkLabelWhatsApp = new System.Windows.Forms.LinkLabel();
+            this.linkLabelPedidos = new System.Windows.Forms.LinkLabel();
+            this.linkLabelCalendario = new System.Windows.Forms.LinkLabel();
+            this.linkLabelFinanPessoal = new System.Windows.Forms.LinkLabel();
+            this.linkLabelFinaneEstoque = new System.Windows.Forms.LinkLabel();
+            this.linkLabelFotos = new System.Windows.Forms.LinkLabel();
+            this.linkLabelClientes = new System.Windows.Forms.LinkLabel();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkLabelWhatsApp
+            // 
+            this.linkLabelWhatsApp.AutoSize = true;
+            this.linkLabelWhatsApp.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.linkLabelWhatsApp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabelWhatsApp.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelWhatsApp.LinkColor = System.Drawing.Color.Black;
+            this.linkLabelWhatsApp.Location = new System.Drawing.Point(101, 220);
+            this.linkLabelWhatsApp.Name = "linkLabelWhatsApp";
+            this.linkLabelWhatsApp.Size = new System.Drawing.Size(220, 42);
+            this.linkLabelWhatsApp.TabIndex = 0;
+            this.linkLabelWhatsApp.TabStop = true;
+            this.linkLabelWhatsApp.Text = "WHATSAPP";
+            this.linkLabelWhatsApp.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelWhatsApp_LinkClicked);
+            // 
+            // linkLabelPedidos
+            // 
+            this.linkLabelPedidos.AutoSize = true;
+            this.linkLabelPedidos.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.linkLabelPedidos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabelPedidos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelPedidos.LinkColor = System.Drawing.Color.Black;
+            this.linkLabelPedidos.Location = new System.Drawing.Point(101, 311);
+            this.linkLabelPedidos.Name = "linkLabelPedidos";
+            this.linkLabelPedidos.Size = new System.Drawing.Size(179, 42);
+            this.linkLabelPedidos.TabIndex = 1;
+            this.linkLabelPedidos.TabStop = true;
+            this.linkLabelPedidos.Text = "PEDIDOS";
+            this.linkLabelPedidos.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelPedidos_LinkClicked);
+            // 
+            // linkLabelCalendario
+            // 
+            this.linkLabelCalendario.AutoSize = true;
+            this.linkLabelCalendario.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.linkLabelCalendario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabelCalendario.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelCalendario.LinkColor = System.Drawing.Color.Black;
+            this.linkLabelCalendario.Location = new System.Drawing.Point(101, 406);
+            this.linkLabelCalendario.Name = "linkLabelCalendario";
+            this.linkLabelCalendario.Size = new System.Drawing.Size(256, 42);
+            this.linkLabelCalendario.TabIndex = 2;
+            this.linkLabelCalendario.TabStop = true;
+            this.linkLabelCalendario.Text = "CALENDÁRIO";
+            this.linkLabelCalendario.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelCalendario_LinkClicked);
+            // 
+            // linkLabelFinanPessoal
+            // 
+            this.linkLabelFinanPessoal.AutoSize = true;
+            this.linkLabelFinanPessoal.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.linkLabelFinanPessoal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabelFinanPessoal.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelFinanPessoal.LinkColor = System.Drawing.Color.Black;
+            this.linkLabelFinanPessoal.Location = new System.Drawing.Point(527, 311);
+            this.linkLabelFinanPessoal.Name = "linkLabelFinanPessoal";
+            this.linkLabelFinanPessoal.Size = new System.Drawing.Size(392, 42);
+            this.linkLabelFinanPessoal.TabIndex = 8;
+            this.linkLabelFinanPessoal.TabStop = true;
+            this.linkLabelFinanPessoal.Text = "FINANÇAS PESSOAIS";
+            this.linkLabelFinanPessoal.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelFinanPessoal_LinkClicked);
+            // 
+            // linkLabelFinaneEstoque
+            // 
+            this.linkLabelFinaneEstoque.AutoSize = true;
+            this.linkLabelFinaneEstoque.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.linkLabelFinaneEstoque.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabelFinaneEstoque.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelFinaneEstoque.LinkColor = System.Drawing.Color.Black;
+            this.linkLabelFinaneEstoque.Location = new System.Drawing.Point(500, 220);
+            this.linkLabelFinaneEstoque.Name = "linkLabelFinaneEstoque";
+            this.linkLabelFinaneEstoque.Size = new System.Drawing.Size(419, 42);
+            this.linkLabelFinaneEstoque.TabIndex = 6;
+            this.linkLabelFinaneEstoque.TabStop = true;
+            this.linkLabelFinaneEstoque.Text = "FINANÇAS E ESTOQUE";
+            this.linkLabelFinaneEstoque.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelFinaneEstoque_LinkClicked);
+            // 
+            // linkLabelFotos
+            // 
+            this.linkLabelFotos.AutoSize = true;
+            this.linkLabelFotos.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.linkLabelFotos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabelFotos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelFotos.LinkColor = System.Drawing.Color.Black;
+            this.linkLabelFotos.Location = new System.Drawing.Point(767, 390);
+            this.linkLabelFotos.Name = "linkLabelFotos";
+            this.linkLabelFotos.Size = new System.Drawing.Size(141, 42);
+            this.linkLabelFotos.TabIndex = 14;
+            this.linkLabelFotos.TabStop = true;
+            this.linkLabelFotos.Text = "FOTOS";
+            this.linkLabelFotos.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelFotos_LinkClicked);
+            // 
+            // linkLabelClientes
+            // 
+            this.linkLabelClientes.AutoSize = true;
+            this.linkLabelClientes.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.linkLabelClientes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabelClientes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelClientes.LinkColor = System.Drawing.Color.Black;
+            this.linkLabelClientes.Location = new System.Drawing.Point(101, 503);
+            this.linkLabelClientes.Name = "linkLabelClientes";
+            this.linkLabelClientes.Size = new System.Drawing.Size(195, 42);
+            this.linkLabelClientes.TabIndex = 12;
+            this.linkLabelClientes.TabStop = true;
+            this.linkLabelClientes.Text = "CLIENTES";
+            this.linkLabelClientes.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelClientes_LinkClicked);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox7.Image = global::DeliciasDaAna.Properties.Resources.iconeFotos;
+            this.pictureBox7.Location = new System.Drawing.Point(695, 374);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(66, 74);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 15;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox8.Image = global::DeliciasDaAna.Properties.Resources.iconetelefone;
+            this.pictureBox8.Location = new System.Drawing.Point(29, 487);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(66, 74);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 13;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox4.Image = global::DeliciasDaAna.Properties.Resources.iconeporcodinheiro;
+            this.pictureBox4.Location = new System.Drawing.Point(455, 295);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(66, 74);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox6.Image = global::DeliciasDaAna.Properties.Resources.iconeDinheiro;
+            this.pictureBox6.Location = new System.Drawing.Point(428, 205);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(66, 74);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 9;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox3.Image = global::DeliciasDaAna.Properties.Resources.iconeCalendário;
+            this.pictureBox3.Location = new System.Drawing.Point(29, 390);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(66, 74);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox2.Image = global::DeliciasDaAna.Properties.Resources.iconePedidos;
+            this.pictureBox2.Location = new System.Drawing.Point(29, 298);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(66, 74);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.Image = global::DeliciasDaAna.Properties.Resources.iconewhatsapp;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 205);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(931, 618);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.linkLabelFotos);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.linkLabelClientes);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.linkLabelFinanPessoal);
+            this.Controls.Add(this.linkLabelFinaneEstoque);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabelCalendario);
+            this.Controls.Add(this.linkLabelPedidos);
+            this.Controls.Add(this.linkLabelWhatsApp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Delícias da Ana";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabelWhatsApp;
+        private System.Windows.Forms.LinkLabel linkLabelPedidos;
+        private System.Windows.Forms.LinkLabel linkLabelCalendario;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.LinkLabel linkLabelFinanPessoal;
+        private System.Windows.Forms.LinkLabel linkLabelFinaneEstoque;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.LinkLabel linkLabelFotos;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.LinkLabel linkLabelClientes;
+    }
+}
+
---
+++ DeliciasDaAna/Formulário Principal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeliciasDaAna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabelWhatsApp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://web.whatsapp.com/");
+        }
+
+        private void linkLabelFinaneEstoque_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/spreadsheets");
+        }
+
+        private void linkLabelPedidos_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/spreadsheets");
+        }
+
+        private void linkLabelFinanPessoal_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/spreadsheets");
+        }
+
+        private void linkLabelCalendario_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://calendar.google.com/calendar");
+        }
+
+        private void linkLabelFotos_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://drive.google.com/");
+        }
+
+        private void linkLabelClientes_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/spreadsheets");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
